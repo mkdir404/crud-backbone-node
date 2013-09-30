5ed86853a60c3fc2aa63921a04f6ef9761aa5076
mkdir404 <ricardo.alba@hotmail.com>
2013-09-29 22:03:22 -0500

    create users

--- public/js/app/usuarios/views/list.js
+++ public/js/app/usuarios/views/list.js
@@ -1,9 +1,43 @@
 define([
   'usuarios/collection/collection',
-  'text!templates/list.html'
-], function(ProjectsCollection, projectsListTemplate){
+  'text!templates/list.html',
+  'usuarios/model/model',
+], function(ProjectsCollection, projectsListTemplate , ModelUsers){
   var ProjectListView = Backbone.View.extend({
     el: $("#contenedor"),
+    events:{
+      'submit .edit-user-form': 'saveUser'
+    },
+    saveUser: function(ev){
+
+      modelUsers =  new ModelUsers;
+      currentDate = new Date();
+      
+      modelUsers.save({
+
+      'nombre'            : $('#nombre').val(),
+      'username'          : $('#username').val(),
+      'email'             : $('#email').val(),
+      'apellido_paterno'  : 'Dame',
+      'apellido_materno'  : 'Perez',
+      'genero'            : $('#genero').val(),
+      'foto'              : 'una futa a una imagen',
+      'fecha_nacimiento'  :  $('#dia').val()+'/'+$('#mes').val()+'/'+$('#anno').val(),
+      'password'          : 'xxxxxx',
+      'fecha_alta'        : currentDate,
+      'rol_k'             : 1
+
+
+    },{
+
+      success:function(usuario){
+      alert('envio completo')
+    }
+  });
+
+      return false;
+
+    },
     initialize: function(){
         
           this.collection = new ProjectsCollection;
